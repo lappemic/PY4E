7265200ff84052d4d07bdcd9e2305def2ff9dcde
lappemic <61876623+lappemic@users.noreply.github.com>
2021-12-23 16:13:03 +0100

    Ex. 8

---
+++ ex_08/ex_08.py
@@ -0,0 +1,15 @@
+han = open('mbox-short.txt')
+
+for line in han:
+    line = line.rstrip()
+    #print('Line', line)
+    #if line == '':
+        #print('Skip Blank')
+        #continue
+    wds = line.split()
+    #print('Words', wds)
+    # Guardian in a compound statement
+    if len(wds) < 3 or wds[0] != 'From':
+        #print('Ignore')
+        continue
+    print(wds[2])
