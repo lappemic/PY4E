df680b7f79923b73f1547803c2852ad86b21f652
lappemic <61876623+lappemic@users.noreply.github.com>
2022-03-14 16:02:01 +0100

    Book exercise 13

--- ex_13_Extracting_Data_from_Web/ex_13_Extracting_Data_from_XML.py
+++ ex_13_Extracting_Data_from_Web/ex_13_Extracting_Data_from_XML.py
@@ -7,37 +7,40 @@ api_key = False
 # api_key = 'AIzaSy___IDByT70'
 # https://developers.google.com/maps/documentation/geocoding/intro
 
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-else :
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+# if api_key is False:
+#     api_key = 42
+#     serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
+# else :
+#     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1: break
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
+url = input('Enter url: ')
+if len(url) < 1:
+    url = "http://py4e-data.dr-chuck.net/comments_42.xml"
 
-    data = uh.read()
-    print('Retrieved', len(data), 'characters')
-    print(data.decode())
-    tree = ET.fromstring(data)
+parms = dict()
+parms['address'] = address
+if api_key is not False: parms['key'] = api_key
+url = serviceurl + urllib.parse.urlencode(parms)
+print('Retrieving', url)
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read()
+print(data)
+print('Retrieved', len(data), 'characters')
+print(data.decode())
+# tree = ET.fromstring(data)
+# counts = tree.findall('.//count')
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
 
-    print('lat', lat, 'lng', lng)
-    print(location)
+# results = tree.findall('result')
+# lat = results[0].find('geometry').find('location').find('lat').text
+# lng = results[0].find('geometry').find('location').find('lng').text
+# location = results[0].find('formatted_address').text
+#
+# print('lat', lat, 'lng', lng)
+# print(location)
