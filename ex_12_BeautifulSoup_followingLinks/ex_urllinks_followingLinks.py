0dcc88cc847ffd68066f2fba21c2f0b58e12e462
lappemic <61876623+lappemic@users.noreply.github.com>
2022-03-14 01:02:40 +0100

    partsaving

--- ex_12_BeautifulSoup_followingLinks/ex_urllinks_followingLinks.py
+++ ex_12_BeautifulSoup_followingLinks/ex_urllinks_followingLinks.py
@@ -11,11 +11,41 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = input('Enter URL: ')
+if len(url) < 1:
+    url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
+counts = int(input('Enter count: '))
+position = int(input('Enter position: '))
 
 # Retrieve all of the anchor tags
 tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+#print(tags)
+print('Retrieving: ', tags[0].get('href', None))
+
+for count in range(counts):
+    # print(tags[position].get('href', None))
+    urln = tags[position].get('href', None)
+    # print(urln)
+    htmln = urllib.request.urlopen(urln, context = ctx).read()
+    soupn = BeautifulSoup(htmln, 'html.parser')
+    tags = soupn('a')
+    print('Retrieving: ', tags[position].get('href', None))
+    # for line in fhand:
+    #     print(line.decode().strip())
+
+
+
+# fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+# for line in fhand:
+#     print(line.decode().strip())
+
+# for tag in tags:
+#     print(tag.get('href', None))
+
+
+
+
+#print(type(tags))
+#print(tags[4])
